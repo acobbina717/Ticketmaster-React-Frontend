1f4302369062a7e8e22d109b5253c177b929b405
Kaven Kim <kavenkim@Kavens-MBP.fios-router.home>
2022-06-02 23:54:09 -0400

    Updated EventReviewPage Component and ReviewCard Component

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,7 +1,64 @@
 import React from "react";
+import {
+  Card,
+  Textarea,
+  Text,
+  Badge,
+  Button,
+  Group,
+  useMantineTheme,
+} from "@mantine/core";
 
-const ReviewCard = () => {
-  return <div>ReviewCard</div>;
-};
+function ReviewCard({ review, id }) {
+  //------------------------
+  // Function to delete a review
+
+  // function handleDeleteClick() {
+  //   fetch(`http://localhost:9292/reviews/${id}`, {
+  //     method: "DELETE",
+  //   });
+
+  //   onDeleteReview(id);
+  // }
+  //---------------------
+
+  //----------------------------
+  // Function to edit a review
+
+  // function handleReviewEdit(e) {
+  //   e.preventDefault();
+
+  //   fetch("http://localhost:9292/reviews", {
+  //     method: "PATCH",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       event_id: `${id}`,
+  //       comment: comment,
+  //     }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((updatedReview) => console.log(updatedReview));
+  // }
+
+  //---------------------------------
+
+  return (
+    <>
+      <Card>
+        <Text size="sm" style={{ lineHeight: 1.5 }}>
+          {review.comment}
+          <Badge position="right" color="dark" variant="light">
+            Edit
+          </Badge>
+          <Badge position="right" color="dark" variant="light">
+            Delete
+          </Badge>
+        </Text>
+      </Card>
+    </>
+  );
+}
 
 export default ReviewCard;
